99df05386be4930dcf07e92c65ab3d5cf9f71907
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-03-15 23:47:21 +0900

    Implement arxiv pdf downloading and number cititations fetching for paper overview

--- fetch_publications.py
+++ fetch_publications.py
@@ -10,6 +10,11 @@ import urllib3
 import os
 from config import booktitles, ROOTPATH, facets
 from lib import scholar
+# https://github.com/lukasschwab/arxiv.py
+# https://github.com/titipata/arxivpy
+import arxiv
+
+import unidecode
 
 def main():
 
@@ -32,7 +37,7 @@ def main():
 
   client = MongoClient('localhost:4321')
   db = client.pub
-  booktitles = ['TREC']
+  booktitles = ['WWW']
 
   # ########################### #
   #      FETCH PUBLICATIONS     #
@@ -53,12 +58,15 @@ def main():
     querier = scholar.ScholarQuerier()
     settings = scholar.ScholarSettings()
     querier.apply_settings(settings)
-    query = scholar.SearchScholarQuery()
+    scholar_query = scholar.SearchScholarQuery()
 
     for pub in results:
       author1 = pub['authors'][0]
+      counter_pub += 1
+      if counter_pub % 50 == 0: print("Processed:", counter_pub)
       title = pub['title'].lower().capitalize().strip('.')
-      paper_info = [pub['_id'], 'false', '0;0', '0', booktitle, pub['ee'], pub['year'], "'%s'" % title, pub['type'], 'author1;author2']
+      paper_info = [pub['_id'], 'false', '-1;-1', '-1', booktitle, pub['ee'], pub['year'], "'%s'" % title, pub['type'], 'author1;author2']
+      no_accent_author1 = unidecode.unidecode(author1)
 
       # Set author info
       authors = ''
@@ -66,11 +74,27 @@ def main():
         authors += f'{author};'
       paper_info[9] = authors.strip(';')
 
+      file_path = f'{ROOTPATH}/data/{database}/{booktitle}/pdf/'
+
       # Have multiple PDF fetch methods: Direct EE link, Arxiv
       if pub['ee'][-4:].lower() == '.pdf': 
         paper_info[1] = 'true'
+        download_pdf(file_path, paper_info[5], database, booktitle, paper_info[0])
+      else:
+        arxiv_query=f'au:{author1}+AND+ti:{title}'
+        articles = arxiv.query(search_query=arxiv_query)
+        if articles:
+          for art in articles:
+            if art['title'].lower().capitalize().strip('.') == title:
+              paper_info[5] = art['pdf_url']
+              paper_info[1] = 'true'
+              arxiv.download(art, file_path)
+              os.rename(f'{file_path}{art["title"]}.pdf', f'{file_path}{paper_info[0]}.pdf')
+
+              print(f'Finished PDF download for {paper_info[0]}')
 
       # Get distinct #entities for total facets
+      # ADD PROPER ENTITIES EXTRACTION
       facets_entities = ''
       for facet in facets:
         entities = fetch_paper_entities(pub['_id'], facet, db)
@@ -81,28 +105,30 @@ def main():
 
       paper_info[2] = facets_entities.strip(';')
 
+      # Only fetch citations if a PDF has been downloaded
+      if paper_info[1] == 'true':      
+        # Get number of citations info
+        scholar_query.set_author(no_accent_author1)
+        scholar_query.set_phrase(title)
+        scholar_query.set_num_page_results(1)
+        querier.send_query(scholar_query)
+        querier.save_cookies()
+
+        # # Print the URL of the first article found
+        if querier.articles and title == querier.articles[0]['title'].lower().capitalize().strip('.'):
+          print(f'Fetched number citations for {paper_info[0]}: {querier.articles[0]["num_citations"]}')
+          paper_info[3] = querier.articles[0]['num_citations']
+
+          # print(querier.articles[0]['title'], ":", querier.articles[0]['num_citations'], querier.articles[0]['url_pdf'])
+        # print(querier.articles[0]['num_citations'], querier.articles[0]['url_pdf'])
+
+
+      # Add paper information to list
       papers.append(paper_info)
       print(f'✓ {pub["_id"]}')
 
-      # Get number of citations info
-      # query.set_author(author1)
-      # query.set_phrase(title)
-      # query.set_num_page_results(1)
-      # querier.send_query(query)
-      # querier.save_cookies()
-
-      # print(querier.articles)
-
-      # # # Print the URL of the first article found
-      # if querier.articles:
-      #   if not title == querier.articles[0]['title'].lower().capitalize().strip('.'):
-      #     print(title)
-      #   print(querier.articles[0]['title'], ":", querier.articles[0]['num_citations'], querier.articles[0]['url_pdf'])
-      # # print(querier.articles[0]['num_citations'], querier.articles[0]['url_pdf'])
-
     # Write papers information to CSV file
     write_arrays_to_csv(papers, booktitle, database, ['paper_id', 'has_pdf', facets_columns, 'number_citations', 'booktitle', 'pdf_url', 'year', 'title', 'type', 'authors'])
-
 
 # Fetch number of named entities for each papers in specific journal with facet
 def fetch_paper_entities(paper_id, facet, db):
@@ -132,6 +158,16 @@ def write_entity_set_file(paper_id, booktitle, entities, database, facet):
   with open(file_path, 'w+') as outputFile:
     for e in entities:
       outputFile.write(f'{e}\n')
+
+def download_pdf(file_path, download_url, database, booktitle, paper_name):
+  os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+  http = urllib3.PoolManager()
+  response = http.request('GET', download_url)
+  file = open(f'{file_path}{paper_name}.pdf', 'wb')
+  file.write(response.data)
+  file.close()
+  print(f'Finished PDF download for {paper_name}')
 
 if __name__=='__main__':
   main()
