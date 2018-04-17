982e9067fc473b3ee8be7e98c0e15d758294ce69
Vliegenthart <daniel.vliegenthart@gmail.com>
2018-04-17 22:51:25 +0900

    Add pdf page width/height ratio information to highlights + add requirements.txt

--- generate_pdf_term_highlights_file.py
+++ generate_pdf_term_highlights_file.py
@@ -8,10 +8,11 @@ import re
 import urllib3
 from operator import itemgetter
 import os
-from config import booktitles, ROOTPATH, facets
+from config import booktitles, ROOTPATH, PDFNLT_PATH, facets
 import unidecode
 import json
 import pprint
+from PIL import Image
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -45,39 +46,43 @@ def main():
 
       pdf_terms_pages = json.load(open(f'{json_path}/{facet}_{file_name}_pdf_terms_pages.json'))
       term_highlights = generate_term_highlights(pdf_terms_pages, file_name, facet)
-      write_highlights_json(term_highlights, file_name)
+      write_highlights_js(term_highlights, file_name)
 
 # Generate array of terms meta-data like position, comment, content and id
 def generate_term_highlights(pdf_terms, file_name, facet):
   highlights = { f'{file_name}.pdf': []}
   number_pages = len(pdf_terms)
+
+  # Get pdf page width/height ratio
+  with Image.open(f'{PDFNLT_PATH}/xhtml/images/{file_name}/{file_name}-01.png') as page:
+    page_width, page_height = page.size
   
   for i1, page_terms in enumerate(pdf_terms):
     # if i1 > 0: break
 
     for i2, term in enumerate(page_terms):
       # if i2 > 2: continue
- 
+
       words_processed = 1
       highlight = { 'content': {'text': term['text']}, 'position': { 'pageNumber': int(term['page_number']) + 1}, 'comment': { 'text': '', 'facet': facet}, 'id': term['id'], 'type': 'automatic' }
 
       # Calculate position boundingRect and word rects
-      bdr = term['pdf_words'][0]['bdr'].split(',')
+      bdr = bdr_to_coord(term['pdf_words'][0]['bdr'].split(','), page_width, page_height)
       if not len(bdr) == 4: continue 
 
-      rects = [{ 'x1': bdr[0], 'x2': bdr[2], 'y1': bdr[1], 'y2': bdr[3], 'width': -1, 'height': -1 }]
+      rects = [{ 'x1': bdr[0], 'x2': bdr[2], 'y1': bdr[1], 'y2': bdr[3], 'width': page_width, 'height': page_height }]
       bounding_rect = rects[0].copy()
 
       if len(term['pdf_words']) > 1:
         for i3, word in enumerate(term['pdf_words']):
           if i3 is 0: continue
 
-          bdr = word['bdr'].split(',')
-          
+          bdr = bdr_to_coord(word['bdr'].split(','), page_width, page_height)
+
           if not len(bdr) == 4: continue 
           words_processed += 1;
 
-          word_rect = { 'x1': bdr[0], 'x2': bdr[2], 'y1': bdr[1], 'y2': bdr[3], 'width': -1, 'height': -1 }
+          word_rect = { 'x1': bdr[0], 'x2': bdr[2], 'y1': bdr[1], 'y2': bdr[3], 'width': page_width, 'height': page_height }
 
           # Update bounding_rect
           if float(word_rect['x1']) < float(bounding_rect['x1']): bounding_rect['x1'] = word_rect['x1']
@@ -102,9 +107,20 @@ def generate_term_highlights(pdf_terms, file_name, facet):
 
   return highlights
 
+# Convert bdr percentage to page coordinates
+def bdr_to_coord(bdr, page_width, page_height):
+  temp = bdr[:]
+  if not len(temp) == 4: return temp
+
+  temp[0] = float(temp[0]) * page_width
+  temp[2] = float(temp[2]) * page_width
+  temp[1] = float(temp[1]) * page_height
+  temp[3] = float(temp[3]) * page_height
+  return temp
+
 # Write the array of highlights to ES6 JS file
 def write_highlights_js(highlights, file_name):
-  file_content = f'// @flow \n\nconst testHighlights = {json.dumps(highlights, indent=2)}'
+  file_content = f'// @flow \n\nconst termHighlights = {json.dumps(highlights, indent=2)};\n\nexport default termHighlights;\n'
   file_path = f'/data/highlight/{file_name}-highlights.js'
   os.makedirs(os.path.dirname(ROOTPATH + file_path), exist_ok=True)
 
